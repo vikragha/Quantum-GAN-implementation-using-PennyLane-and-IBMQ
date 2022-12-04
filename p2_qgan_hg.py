0813786d0f455fdf5e10109cf2c74aafbf95fb43
vikragha <55603725+vikragha@users.noreply.github.com>
2022-12-04 18:15:46 -0500

    Update p2_qgan_hg.py

--- p2_qgan_hg.py
+++ p2_qgan_hg.py
@@ -308,7 +308,7 @@ if __name__ == '__main__':
     parser.add_argument('--quantum', type=bool, default=True, help='choose to use quantum gan with hybrid generator')
     parser.add_argument('--patches', type=int, default=1, help='number of quantum circuit patches')
     parser.add_argument('--layer', type=int, default=1, help='number of repeated variational quantum layer')
-    parser.add_argument('--qubits', type=int, default=4, help='number of qubits and dimension of domain labels')
+    parser.add_argument('--qubits', type=int, default=8, help='number of qubits and dimension of domain labels')
  
     # Model configuration.
     parser.add_argument('--z_dim', type=int, default=8, help='dimension of domain labels')
