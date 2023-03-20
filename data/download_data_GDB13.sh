fb2a37d4c04a7e35571186effa9fb9314b1047d3
Vikram <55603725+vikragha@users.noreply.github.com>
2023-03-19 20:17:58 -0400

    Update download_data_GDB13.sh

--- data/download_data_GDB13.sh
+++ data/download_data_GDB13.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 wget http://gdbtools.unibe.ch:8080/cdn/gdb13.tgz
-tar xvzfO gdb13.tgz --exclude 13.smi | gzip > gdb-12.smi.gz
-tar xvzfO gdb13.tgz | gzip > gdb-13.smi.gz
+tar xvzfO gdb13.tgz --exclude 13.smi
 
 
 wget https://github.com/gablg1/ORGAN/raw/master/organ/NP_score.pkl.gz
