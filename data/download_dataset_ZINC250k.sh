62af0580c026ac1fd4d86e8d5c14d4c212cfe472
Vikram <55603725+vikragha@users.noreply.github.com>
2023-03-19 20:15:29 -0400

    Update download_dataset_ZINC250k.sh

--- data/download_dataset_ZINC250k.sh
+++ data/download_dataset_ZINC250k.sh
@@ -1,2 +1,11 @@
+#!/bin/bash
 wget http://cactus.nci.nih.gov/download/nci/NCISMA99.sdz
 gzip -dc NCISMA99.sdz | awk '{print $2 " NCI" $1}' | sed "s/\[\([BCNOPSF]\)\]/\1/g" | gzip > nci-250k.smi.gz
+
+wget https://github.com/gablg1/ORGAN/raw/master/organ/NP_score.pkl.gz
+wget https://github.com/gablg1/ORGAN/raw/master/organ/SA_score.pkl.gz
+
+
+
+
+
